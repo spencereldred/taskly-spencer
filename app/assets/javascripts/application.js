c4321cabceb403f8dc73b5643446520b74b1f0ab
Spencer Eldred <eldredspencer@gmail.com>
2014-08-14 09:03:41 -0600

    Add clear form button, erases error messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
   var my_textfield = $('.my_textfield')
   var my_button = $('.my_button')
-
+  var clear_task_form = $('.clear_task_form')
 
   my_textfield.on("keyup", function(){
     console.log("task_list_form keyup event logged");
@@ -23,8 +23,18 @@ $(document).ready(function(){
       my_button.prop("disabled", true);
 
       $('#task_list_message').append('\<p class="prepended error">Can\'t leave textfield blank.</p>');
+      $('#task_message').append('\<p class="prepended error">Can\'t leave textfield blank.</p>');
     }
   });
+
+  clear_task_form.on("click", function(){
+    console.log("clear task form button event logged");
+    my_textfield.val("")
+    $('.prepended').remove();
+    my_button.prop("disabled", true);
+
+  })
+
 });
 
 
